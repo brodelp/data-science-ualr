2b81edb5e98c74830d38d9f96c45173db671ad5d
brodelp <brandon@bpenny.dev>
2024-04-25 23:29:41 +0000

    render iframe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.currentTarget);
+  //   const id = formData.get('columnKey') as string
+  //   const value = formData.get('columnVal') as string | number
+  //   console.log(id)
+  //   console.log(value)
+    
+  //   setColumnFilters({colKey: id, colVal: value});
+  // };
+
+  const [iframeURL, setIframeURL] = useState('/api/iframe')
+
+  // const handleGetMap = async () => {
+
+  //   const iframe = await axios.get('/api/iframe')
+    
+  //   setIframeURL(flatData)
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h3>Iframes in React</h3>
+      <iframe src={iframeURL}></iframe>
+      {/* <button
+        onClick={handleGetMap}
+      >
+      </button> */}
+  </div>
   )
 }
 
